43d306e9045d91ee7677eca2b47790f3bb62ad05
Alex Irion <alexirion10@gmail.com>
2017-05-03 16:04:50 -0500

    retrieving student email addresses

--- elit3_h4ck.py
+++ elit3_h4ck.py
@@ -1,6 +1,7 @@
 import sys
+import argparse                    #parsing command line args
+from bs4 import BeautifulSoup      #webscraping
 from subprocess import call
-import argparse
 
 # get student names from canvas / finger cs machines
 # look up student email addresses on student api directory
@@ -8,8 +9,36 @@ import argparse
 # send advising bar 
 
 ####### HOW TO RUN THIS FILE ###########
-# elit3_h4ck -file filename.txt
-# elit3_h4ck -n
+# elit3_h4ck -file filename.txt         #uses email addresses already in text file
+# elit3_h4ck -n                         #pulls email addresses from canvas
+
+def find_canvas_student_names():
+    student_names = []
+    student_names.append('hello world')
+    student_names.append('alex irion')
+    student_names.append('tony sampson')
+    return student_names
+
+def ut_directory_lookup(student_names):
+    # GET request to UT directory, webscrape for email address
+    for name in student_names:
+        name_array = name.split()
+        request_url = 'https://directory.utexas.edu/index.php?q=' # Set destination URL here
+        request_url += name_array[0] + '+' + name_array[1]
+        request_url += '&scope=all&submit=Search'
+        print(request_url)
+
+        # page = requests.get(request_url)
+        # soup = BeautifulSoup(page.content, "html.parser")
+
+        # table = soup.find_all('table', class_='dir_info')
+        # for row in table:
+        #     cells = row.find_all('tr')
+        #     for tr in cells:
+        #         tds = tr.find_all('td')
+        #         if 'Email:' in str(tds[0].text):
+        #             email_list.append( str(tds[1].text)[1:-1] )
+
 
 ####################################
 # Parse args to determine where to get email addresses
@@ -21,8 +50,8 @@ args = parser.parse_args()
 email_list = []
 
 if args.file:
+    #gets email addresses from a text file
     print("Sending phishing emails to addresses in file ~" + args.file + "~")
-    # if a file is being used.....
     file = open(args.file, "r", encoding="utf-8")
     try:
         email_list = file.read().splitlines()
@@ -31,48 +60,25 @@ if args.file:
 
 if args.n:
     print("Sending phishing emails to addresses found from UTCS finger, Canvas groups, and UT directory")
-
-print(email_list)
-
+    # run finger on UTCS machines
+    # find student names from canvas api
+    student_names = find_canvas_student_names()
+    #find student email address from student name
+    ut_directory_lookup(student_names)
 
 
 ####################################
 # send email to list of addresses
 ####################################
-#
-# Import smtplib for the actual sending function
-# import smtplib
-
-# # Import the email modules we'll need
-# from email.mime.text import MIMEText
-
-# # Open a plain text file for reading.  For this example, assume that
-# # the text file contains only ASCII characters.
-# fp = open(textfile, 'rb')
-# # Create a text/plain message
-# msg = MIMEText(fp.read())
-# fp.close()
-
-# # me == the sender's email address
-# # you == the recipient's email address
-# msg['Subject'] = 'The contents of %s' % textfile
-# msg['From'] = me
-# msg['To'] = you
-
-# # Send the message via our own SMTP server, but don't include the
-# # envelope header.
-# s = smtplib.SMTP('localhost')
-# s.sendmail(me, [you], msg.as_string())
-# s.quit()
 
 
-#
+
 #####################################
 # listener for clicked submit buttons on our fake web server???
 ####################################
-#
+# call(['python3', 'evil_server.py'])
 
-#
+
 #####################################
 # save/use stolen credentials
 ####################################
--- send_mail.py
+++ send_mail.py
@@ -22,21 +22,51 @@
 # # Send the message via our own SMTP server, but don't include the
 # # envelope header.
 # s = smtplib.SMTP('localhost')
-# s.sendmail(me, [you], msg.as_string())
+# s.sendmail(me, [you], msg.as_string()
 # s.quit()
+# 
+## ///////////////////////////////////////////
+import requests
+#connect to canvas api to get student names in courses
+# access_token = '1017~DSH05LRqWUsyMocT6hCsMHNuX2gA6Yl6NJlt0Zwnsde0uj9xC8bT66YhlmSgva6g'
+# #1017~DSH05LRqWUsyMocT6hCsMHNuX2gA6Yl6NJlt0Zwnsde0uj9xC8bT66YhlmSgva6g
+# canvas_request = 'https://canvas.instructure.com/api/v1/courses?q=enrollment_state=active&access_token=' + access_token
+# r = requests.get(canvas_request)
+# something = r.json()
+# for line in something:
+#     print(line)
 
 
-from urllib.parse import urlencode
-import urllib.request
 
-request_url = 'https://directory.utexas.edu/index.php?q=' # Set destination URL here
-request_url += 'alex+irion'
-request_url += '&scope=all&submit=Search'
+### ///////////////////////////////////////////
+student_names = ['alex irion', 'tony sampson', 'wes draper', 'sam langner', 'chad baldwin']  
+student_emails = []
 
-# https://directory.utexas.edu/index.php?q=alex+irion&scope=all&submit=Search
-print(request_url)
+from bs4 import BeautifulSoup
 
-json = urllib.request.urlopen(request_url).read()
-# urlopen(request).read().decode()
-print(json)
+for name in student_names:
+    name_array = name.split()
+    request_url = 'https://directory.utexas.edu/index.php?q=' # Set destination URL here
+    request_url += name_array[0] + '+' + name_array[1]
+    request_url += '&scope=all&submit=Search'
+    #print(request_url)
+
+    page = requests.get(request_url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    print(soup.prettify())
+
+    table = soup.find_all('table', class_='dir_info')
+
+    for row in table:
+        cells = row.find_all('tr')
+        for tr in cells:
+            tds = tr.find_all('td')
+            if 'Email:' in str(tds[0].text):
+                student_emails.append( str(tds[1].text) )
+
+
+print(student_emails)
+
+
+
 
